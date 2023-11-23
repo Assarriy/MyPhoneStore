29a9996a7bfeca3c630a4f1e01692af6c032e0fa
assarriy <wibowoassariy23@gmail.com>
2023-11-23 20:24:27 +0700

    Fix Font

--- app/src/main/java/com/assarriy/myphonestore/MainActivity.kt
+++ app/src/main/java/com/assarriy/myphonestore/MainActivity.kt
@@ -3,12 +3,13 @@ package com.assarriy.myphonestore
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var rvIphone: RecyclerView
-
+    private val list = ArrayList<Phone>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +17,26 @@ class MainActivity : AppCompatActivity() {
 
         rvIphone = findViewById(R.id.rv_iphone)
         rvIphone.setHasFixedSize(true)
+        list.addAll(getListIphone())
+        showRecyclerList()
+    }
+
+    private fun getListIphone(): ArrayList<Phone> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPrice = resources.getStringArray(R.array.data_price)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val listIphone = ArrayList<Phone>()
+        for (i in dataName.indices){
+            val phone = Phone(dataName[i], dataDescription[i], dataPrice[i], dataPhoto.getResourceId(i, -1))
+            listIphone.add(phone)
+        }
+        return listIphone
+    }
+
+    private fun showRecyclerList() {
+        rvIphone.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListIphoneAdapter(list)
+        rvIphone.adapter = listHeroAdapter
     }
 }
