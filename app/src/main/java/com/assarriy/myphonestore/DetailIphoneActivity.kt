eb64a49b4d1d33e64018eb866b8982fcec17f7ce
assarriy <wibowoassariy23@gmail.com>
2023-11-24 16:31:26 +0700

    Adding Detail Screen

---
+++ app/src/main/java/com/assarriy/myphonestore/DetailIphoneActivity.kt
@@ -0,0 +1,11 @@
+package com.assarriy.myphonestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailIphoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_iphone)
+    }
+}
