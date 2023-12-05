ff27db12fdf92bb632433bf8824444b8972e1605
assarriy <wibowoassariy23@gmail.com>
2023-12-05 12:31:57 +0700

    Adding Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("androidx.core:core-splashscreen:1.0.0")
 }
